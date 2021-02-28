ef4c304ebcbe60ea5355fff252cdb7a1081b4b39
NadaAlyousef92 <nada.alyousef92@gmail.com>
2021-02-28 18:12:02 +0200

    data

--- js/app.js
+++ js/app.js
@@ -1,34 +1,34 @@
 'use strict';
 
 // Cart Constructor
-const Cart = function (items) {
-    this.items=items;
+const Cart = function(items) {
+    this.items = items;
 };
 
 // Create new CartItem and add it to this.item
-Cart.prototype.addItem = function (product, quantity) {
+Cart.prototype.addItem = function(product, quantity) {
     this.items.push(new CartItem(product, quantity));
 };
 
 // Save the contents of the carts to localStorage
-Cart.prototype.SaveToLocalStorage = function (){
+Cart.prototype.SaveToLocalStorage = function() {
     localStorage.setItem('cart', JSON.stringify(this.item));
 };
 
 // Remove one item from the cart
-Cart.prototype.removeItem = function (item){
+Cart.prototype.removeItem = function(item) {
     const index = this.items.indexOf(item);
-    this.items.splice(index,1);
+    this.items.splice(index, 1);
 };
 
 // Passing Parameters
-let CartItem = function (product, quantity){
+let CartItem = function(product, quantity) {
     this.product = product;
     this.quantity = quantity;
 };
 
 // Product Constructor
-const Product = function (filePath, name){
+const Product = function(filePath, name) {
     this.filePath = filePath;
     this.name = name;
     Product.allProducts.push(this);
@@ -38,15 +38,15 @@ Product.allProducts = [];
 
 // The Catalog
 function generateCatalog() {
-    new Product('images/laptop/DellLaptop/dell-1.jpg','Dell');
-    new Product('images/laptop/HpLaptop/hp-1.jpg','hp');
-    new Product('images/laptop/LenovoLaptop/lenovo-1.jpg','lenovo');
-    new Product('images/phones/ApplePhone/iphone-1.jpg','iphone');
-    new Product('images/phones/GooglePixel/google-pixel-1.jpg','google');
-    new Product('images/phones/HuaweiPhone/huawei-1.jpg','huawei');
-    new Product('images/tablet/AppleTablet/apple-tablet-1.jpg','apple-tablet');
-    new Product('images/tablet/HuaweiTablet/huawei-tablet-1.jpg','huawei-tablet');
-    new Product('images/tablet/SamsungTablet/galaxy-tablet-1.jpg','galaxy-tablet');
+    new Product('images/laptop/DellLaptop/dell-1.jpg', 'Dell');
+    new Product('images/laptop/HpLaptop/hp-1.jpg', 'hp');
+    new Product('images/laptop/LenovoLaptop/lenovo-1.jpg', 'lenovo');
+    new Product('images/phones/ApplePhone/iphone-1.jpg', 'iphone');
+    new Product('images/phones/GooglePixel/google-pixel-1.jpg', 'google');
+    new Product('images/phones/HuaweiPhone/huawei-1.jpg', 'huawei');
+    new Product('images/tablet/AppleTablet/apple-tablet-1.jpg', 'apple-tablet');
+    new Product('images/tablet/HuaweiTablet/huawei-tablet-1.jpg', 'huawei-tablet');
+    new Product('images/tablet/SamsungTablet/galaxy-tablet-1.jpg', 'galaxy-tablet');
 }
 
 generateCatalog();
--- js/catalog.js
+++ js/catalog.js
@@ -8,50 +8,50 @@ mainEl.appendChild(sectionEl);
 const cart = new Cart([]);
 //load to dropdown
 function populateForm() {
-  const selectElement = document.getElementById('items');
-  for (let i in Product.allProducts) {
-    let optionEl = document.createElement('option');
-    selectElement.appendChild(optionEl);
-    optionEl.textContent = Product.allProducts[i].name;
-    optionEl.value = Product.allProducts[i].name;
-  }
+    const selectElement = document.getElementById('items');
+    for (let i in Product.allProducts) {
+        let optionEl = document.createElement('option');
+        selectElement.appendChild(optionEl);
+        optionEl.textContent = Product.allProducts[i].name;
+        optionEl.value = Product.allProducts[i].name;
+    }
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  addSelectedItemToCart();
-  cart.saveToLocalStorage();
-  updateCounter();
-  updateCartPreview();
-  confirmMsg();
+    event.preventDefault();
+    addSelectedItemToCart();
+    cart.saveToLocalStorage();
+    updateCounter();
+    updateCartPreview();
+    confirmMsg();
 }
 
 function confirmMsg() {
-  sectionEl.id = 'message';
-  sectionEl.textContent = 'Added to Cart!';
+    sectionEl.id = 'message';
+    sectionEl.textContent = 'Added to Cart!';
 }
 
 //Adding the selected item and quantity to the cart
 function addSelectedItemToCart() {
-  let selectElementItem = document.getElementById('items');
-  let selectIndexItem = selectElementItem.selectedIndex;
-  let product = selectElementItem.options[selectIndexItem].value;
-  let quantity = document.getElementById('quantity').value;
-  cart.addItem(product, quantity);
+    let selectElementItem = document.getElementById('items');
+    let selectIndexItem = selectElementItem.selectedIndex;
+    let product = selectElementItem.options[selectIndexItem].value;
+    let quantity = document.getElementById('quantity').value;
+    cart.addItem(product, quantity);
 }
 
 //Update the cart count in the header nav with the number of items in the Cart
 function updateCounter() {
-  let updateEl = document.getElementById('itemCount');
-  updateEl.textContent = cart.items.length;
+    let updateEl = document.getElementById('itemCount');
+    updateEl.textContent = cart.items.length;
 }
 
 //Show item & quantity in the cart preview div
 function updateCartPreview() {
-  let divEl = document.getElementById('cartContents');
-  let articleEl = document.createElement('article');
-  divEl.appendChild(articleEl);
-  articleEl.textContent = `The Product: ${cart.items[[cart.items.length - 1]].product}, The Quantity: ${cart.items[[cart.items.length - 1]].quantity}`;
+    let divEl = document.getElementById('cartContents');
+    let articleEl = document.createElement('article');
+    divEl.appendChild(articleEl);
+    articleEl.textContent = `The Product: ${cart.items[[cart.items.length - 1]].product}, The Quantity: ${cart.items[[cart.items.length - 1]].quantity}`;
 }
 
 const catalogForm = document.getElementById('catalog');
