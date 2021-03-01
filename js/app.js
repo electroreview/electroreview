d986a6e390722f465b4daef88894e794aa07fb96
MaisDaoud <eng.omosh@gmail.com>
2021-03-01 15:05:17 +0200

    functionality fix

--- js/app.js
+++ js/app.js
@@ -1,52 +1,44 @@
 'use strict';
 
-// Cart Constructor
-const Cart = function(items) {
+const Cart = function (items) {
     this.items = items;
 };
 
-// Create new CartItem and add it to this.item
-Cart.prototype.addItem = function(product, quantity) {
+Cart.prototype.addItem = function (product, quantity) {
     this.items.push(new CartItem(product, quantity));
 };
 
-// Save the contents of the carts to localStorage
-Cart.prototype.SaveToLocalStorage = function() {
-    localStorage.setItem('cart', JSON.stringify(this.item));
+Cart.prototype.saveToLocalStorage = function () {
+    localStorage.setItem('cart', JSON.stringify(this.items));
 };
 
-// Remove one item from the cart
-Cart.prototype.removeItem = function(item) {
+Cart.prototype.removeItem = function (item) {
     const index = this.items.indexOf(item);
     this.items.splice(index, 1);
 };
 
-// Passing Parameters
-let CartItem = function(product, quantity) {
+let CartItem = function (product, quantity) {
     this.product = product;
     this.quantity = quantity;
 };
 
-// Product Constructor
-const Product = function(filePath, name) {
+const Product = function (filePath, name) {
     this.filePath = filePath;
     this.name = name;
     Product.allProducts.push(this);
 };
-
 Product.allProducts = [];
 
-// The Catalog
 function generateCatalog() {
-    new Product('images/laptop/DellLaptop/dell-1.jpg', 'Dell');
-    new Product('images/laptop/HpLaptop/hp-1.jpg', 'hp');
-    new Product('images/laptop/LenovoLaptop/lenovo-1.jpg', 'lenovo');
-    new Product('images/phones/ApplePhone/iphone-1.jpg', 'iphone');
-    new Product('images/phones/GooglePixel/google-pixel-1.jpg', 'google');
-    new Product('images/phones/HuaweiPhone/huawei-1.jpg', 'huawei');
-    new Product('images/tablet/AppleTablet/apple-tablet-1.jpg', 'apple-tablet');
-    new Product('images/tablet/HuaweiTablet/huawei-tablet-1.jpg', 'huawei-tablet');
-    new Product('images/tablet/SamsungTablet/galaxy-tablet-1.jpg', 'galaxy-tablet');
+    new Product('assets/laptop/DellLaptop/dell-1.jpg', 'dell laptop');
+    new Product('assets/laptop/HpLaptop/hp-1.jpg', 'hp laptop');
+    new Product('assets/laptop/LenovoLaptop/lenovo-1.jpg', 'lenovo laptop');
+    new Product('assets/phones/ApplePhone/iphone-1.jpg', 'iphone mobile');
+    new Product('assets/phones/GooglePixel/google-pixel-1.jpg', 'google pixel mobile');
+    new Product('assets/phones/HuaweiPhone/huawei-1.jpg', 'huawei mobile');
+    new Product('assets/tablet/AppleTablet/apple-tablet-1.jpg', 'apple tablet');
+    new Product('assets/tablet/HuaweiTablet/huawei-tablet-1.jpg', 'huawei tablet');
+    new Product('assets/tablet/SamsungTablet/galaxy-tablet-1.jpg', 'samsung tablet');
 }
 
 generateCatalog();
--- js/cart.js
+++ js/cart.js
@@ -1,73 +1,125 @@
 'use strict';
-//create event listener
-const table = document.getElementById('cart');
+
+let table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
 let cart;
-//create a function to load cart
+
 function loadCart() {
     const cartItems = JSON.parse(localStorage.getItem('cart')) || [];
     cart = new Cart(cartItems);
 }
-//create a function to remove item from cart
-function removeItemFromCart(event) {
 
-    let removeArr = document.getElementsByTagName('a');
-
-    for (let i = 0; i < removeArr.length; i++) {
-        if (event.target.id === `remove${i}`) {
-            cart.removeItem(cart.items[i]);
-            break;
-        }
-    }
-    // to save in local storage
-    cart.saveToLocalStorage();
-    renderCart();
-}
-// create function to clear cart
-function clearCart() {
-    let tbEl = document.getElementsByTagName('tbody')[0];
-    tbEl.textContent = '';
-}
-// create function to show item in cart
-function showCart() {
-    let tbEl = document.getElementsByTagName('tbody')[0];
-
-    for (let i = 0; i < cart.items.length; i++) {
-
-        let trEl = document.createElement('tr');
-        tbEl.appenchild(trEl);
-
-        let tdDelete = document.createElement('td');
-        let tdElItem = document.createElement('td');
-        trEl.appendChild(tdDelete);
-
-        let tdQuantity = document.createElement('td');
-        let tdimage = document.createElement('td');
-        trEl.appendChild(tdQuantity);
-
-        tdDelete.innerHTML = `<a id="remove${i}">X</a>`;
-        tdElItem.textContent = cart.items[i].product;
-        trEl.appendChild(tdElItem);
-
-        tdQuantity.textContent = cart.items[i].quantity;
-        let imgEl = document.createElement('img');
-        imgEl.id = 'itemImg';
-        tdimage.appendChild(imgEl);
-
-        trEl.appendChild(tdimage);
-        for (let j = 0; j < Product.allProducts.length; j++) {
-            if (cart.items[i].product === Product.allProducts[j].name) {
-                imgEl.src = `${Product.allProducts[j].filePath}`;
-                break;
-            }
-        }
-    }
-}
-// function to render all functions
 function renderCart() {
     loadCart();
     clearCart();
     showCart();
 }
 
+function fillForm() {
+    const formArr = ['Name', 'Street', 'City', 'State', 'ZIP code', 'Phone Number'];
+    let mainEl = document.getElementsByTagName('main')[0];
+    
+    let sectionEl = document.createElement('section');
+    mainEl.appendChild(sectionEl);
+    
+    let formEl = document.createElement('form');
+    sectionEl.appendChild(formEl);
+    
+    let fieldsetEl = document.createElement('fieldset');
+    formEl.appendChild(fieldsetEl);
+    
+    let legendEl = document.createElement('legend');
+    fieldsetEl.appendChild(legendEl);
+
+    legendEl.textContent = 'Add your Info';
+    
+    for (let i = 0; i < formArr.length; i++) {
+      let pEl = document.createElement('p');
+      fieldsetEl.appendChild(pEl);
+      let labelEl = document.createElement('label');
+      pEl.appendChild(labelEl);
+      labelEl.for = formArr[i];
+      labelEl.textContent = formArr[i];
+      let inputEl = document.createElement('input');
+      pEl.appendChild(inputEl);
+      inputEl.type = 'text';
+      inputEl.id = formArr[i];
+      inputEl.placeholder = formArr[i];
+    }
+    
+    let pEl = document.createElement('p');
+    fieldsetEl.appendChild(pEl);
+    
+    let labelEl = document.createElement('label');
+    pEl.appendChild(labelEl);
+    labelEl.for = 'creditCard';
+    labelEl.textContent = 'Credit Card Number:';
+    
+    let inputEl = document.createElement('input');
+    pEl.appendChild(inputEl);
+    inputEl.type = 'number';
+    inputEl.id = 'creditCard';
+    inputEl.placeholder = 'Enter 16 digits';
+    inputEl.maxLength = 16;
+    
+    let buttonEl = document.createElement('button');
+    fieldsetEl.appendChild(buttonEl);
+    buttonEl.textContent = 'Process Order';
+  
+  }
+
+function clearCart() {
+    let tbEl = document.getElementsByTagName('tbody')[0];
+    tbEl.textContent ='';
+}
+
+function showCart() {
+   
+
+  let tbEl = document.getElementsByTagName('tbody')[0];
+  for (let i = 0; i < cart.items.length; i++) {
+    let trEl = document.createElement('tr');
+    tbEl.appendChild(trEl);
+    
+    let tdElDelete = document.createElement('td');
+    let tdElItem = document.createElement('td');
+    trEl.appendChild(tdElDelete);
+
+    let tdElQuantity = document.createElement('td');
+    let tdElPic = document.createElement('td');
+    trEl.appendChild(tdElQuantity);
+
+    tdElDelete.innerHTML = `<a id="remove${i}">X</a>`;
+    tdElItem.textContent = cart.items[i].product;
+    trEl.appendChild(tdElItem);
+
+    tdElQuantity.textContent = cart.items[i].quantity;
+    let imgEl = document.createElement('img');
+    imgEl.id = 'itemImg';
+    tdElPic.appendChild(imgEl);
+
+    trEl.appendChild(tdElPic);
+    for (let m = 0; m < Product.allProducts.length; m++) {
+      if (cart.items[i].product === Product.allProducts[m].name) {
+        imgEl.src = `${Product.allProducts[m].filePath}`;
+        break;
+      }
+    }
+  }
+}
+
+function removeItemFromCart(event) {
+let removeArr = document.getElementsByTagName('a');
+
+for (let i = 0; i < removeArr.length; i++) {
+  if (event.target.id === `remove${i}`) {
+    cart.removeItem(cart.items[i]);
+    break;
+  }
+}
+  cart.saveToLocalStorage();
+  renderCart();
+}
+
 renderCart();
+fillForm();
